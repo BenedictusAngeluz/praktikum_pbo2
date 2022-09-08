ce25fc5dcac8afc1e50d643407c2ba9dd9e2276e
Benedictus Angelo <100206585+BenedictusAngeluz@users.noreply.github.com>
2022-09-08 23:25:50 +0700

    Inheritance

---
+++ bangunruang/Bola.java
@@ -0,0 +1,19 @@
+package bangunruang;
+
+public class Bola extends BangunRuang {
+    private float r;
+    private float phi;
+
+    public Bola(float r) {
+        this.r = r;
+        this.phi = 22 / 7;
+    }
+
+    public void hitungLuas() {
+        this.luas = 4 * phi * r * r;
+    }
+
+    public void hitungVolume() {
+        this.volume = 4 / 3 * phi * r * r * r;
+    }
+}
